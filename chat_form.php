477c692159ce30ce546350d150f39ddf12101874
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-11 20:25:40 +0300

    Update chat_form.php

--- chat_form.php
+++ chat_form.php
@@ -8,10 +8,13 @@
 </head>
 <body>
     <?php
+    // print_r($_SERVER['SERVER_ADDR']);
+    // print_r($_SERVER['HTTP_USER_AGENT']);
     if(!empty($_POST['mes'])){
-        file_put_contents("chat.txt",$_POST['name'].":".$_POST['mes']."\n",FILE_APPEND);
+        file_put_contents("chat.txt",$_SERVER['HTTP_USER_AGENT'].":".$_SERVER['SERVER_ADDR'].":".$_POST['name'].":".$_POST['mes']."\n",FILE_APPEND);
     }
   
+    
 
     ?>
       <form action='?' method='post'>
