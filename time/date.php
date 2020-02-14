ab85b4e7207b07ef33b917fe2ecba25288124535
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-14 21:09:54 +0300

    Create date.php

---
+++ time/date.php
@@ -0,0 +1,11 @@
+<?php
+echo date("F j, Y, g:i a")."<br>";
+echo date("m.d.y")."<br>";
+echo date ("j, b, Y")."<br>";
+echo date("Ymd")."<br>";
+echo date('h-i-s, j-m-y, it is w Day z')."<br>";
+echo date('\i\t \i\s \t\h\e\ js \d\a\y')."<br>";
+echo date("D M j:i:s T Y")."<br>";
+echo date('H:m:s \m \i\s \m\o\n\t\h')."<br>";
+echo date("H:i:s")."<br>";
+?>
