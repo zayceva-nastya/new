db21e4bb59a4f13ec275956d0688a41dfae4ef00
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-14 21:10:01 +0300

    Create mktime.php

---
+++ time/mktime.php
@@ -0,0 +1,7 @@
+<?php
+echo date("M-d-Y",mktime(0,0,0,12,20,2007))."<br>";
+echo date("M-d-Y",mktime(0,0,0,12,32,2007))."<br>";
+echo date("M-d-Y",mktime(0,0,0,13,1,2007))."<br>";
+echo date("M-d-Y",mktime(0,0,0,1,1,2007))."<br>";
+echo date("M-d-Y",mktime(0,0,0,1,1,07))."<br>";
+?>
