597c403dffe494d52ede08c3d093a023e664e9e8
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-03-03 21:24:51 +0300

    Update filter.php

--- reg1/filter.php
+++ reg1/filter.php
@@ -1,31 +1,16 @@
 <?php
-print_r ($_POST['mail']);
-$file=file_get_contents('list.txt');
-// echo $file;
-$str=preg_split("/[\s,]+/",$file);
-// print_r ($str);
-$pat="/^[0-1]$/";
-foreach ($str as $key => $value) {
-if ((preg_match_all($pat,$key))){
-    // echo $value;
-    
+// print_r ($_POST['mail']);
+$file=file('list.txt');
+foreach($file as &$word){
+    $word=trim($word);
 }
-}
-// if(preg_match_all("/\b($str[0])\b|\b($str[1])\b/",$_POST['mail'])){
-// echo "вы говорите плохие слова";
-// }else{
-    // echo "отлично";
-// }
-// 
-// $reg = implode(';', $str);
-// $reg="дурак редиска ";
-// if(preg_match("/^(\w+$reg)$/i",$_POST['mail'])){
-    // foreach($str as $values){
-        // echo $values;
-    // if(preg_match("/^(\s+\b$values\b)$/i",$_POST['mail'])){
-    // echo "good";
-// }else{
-    // echo "bad";
-// }}
 
+$word = implode('|', $file);
+
+if(preg_match("/$word/i",$_POST['mail'])){
+   
+    echo " НЕ ругайся";
+}else{
+    echo "Отлично";
+}
 ?>
