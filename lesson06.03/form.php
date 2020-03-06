dd97677ea218965ee3295a431badfe590a46ea53
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-03-06 21:24:00 +0300

    Create form.php

---
+++ lesson06.03/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="slovo.php" method="post">
+
+<textarea name="mail" id="" cols="30" rows="10"></textarea>
+<input type="text" name="word">
+<input type="submit">
+    </form>
+</body>
+</html>
