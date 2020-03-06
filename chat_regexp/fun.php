d3bf43c9f144032260086f0ec916ff999ae54811
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-03-06 16:37:36 +0300

    Create fun.php

---
+++ chat_regexp/fun.php
@@ -0,0 +1,43 @@
+<?php
+
+function bb_code($text)
+{
+    $pat = [
+        '/\[b\](.*)\[\/b\]/',
+        '/\[i\](.*)\[\/i\]/',
+        '/\[u\](.*)\[\/u\]/'
+    ];
+
+    $img = [
+        "<b>$1</b>",
+        "<i>$1</i>",
+        "<u>$1</u>"
+    ];
+
+    return  preg_replace($pat, $img, $text);
+}
+
+function smile($text){
+   
+    $pat=[
+        '/(\:\-\))|(\:\))/',
+        '/(\:\-\()|(\:\()/'
+    ];
+
+    $img=[
+        "<img src='rrr.png' height='15'>",
+        "<img src='s.jpg' height='15'>"
+    ];
+
+    return preg_replace($pat, $img, $text);
+  
+}
+
+function cens($text){
+    $pat="/дурак|идиот/";
+    if(preg_match($pat,$text)){
+        return "Ругаться не красиво!";
+    }else{
+        return $text;
+    }
+}
