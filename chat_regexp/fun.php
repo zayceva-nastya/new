4cf62e9153002836db863c9d41d267dcb57b2ecd
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-03-06 21:23:43 +0300

    Update fun.php

--- chat_regexp/fun.php
+++ chat_regexp/fun.php
@@ -5,39 +5,60 @@ function bb_code($text)
     $pat = [
         '/\[b\](.*)\[\/b\]/',
         '/\[i\](.*)\[\/i\]/',
-        '/\[u\](.*)\[\/u\]/'
+        '/\[u\](.*)\[\/u\]/',
+        '/\[img\](.*)\[\/img\]/'
+
     ];
 
     $img = [
         "<b>$1</b>",
         "<i>$1</i>",
-        "<u>$1</u>"
+        "<u>$1</u>",
+        "<img src='$1'>"
     ];
 
     return  preg_replace($pat, $img, $text);
 }
 
-function smile($text){
-   
-    $pat=[
+function smile($text)
+{
+
+    $pat = [
         '/(\:\-\))|(\:\))/',
         '/(\:\-\()|(\:\()/'
     ];
 
-    $img=[
+    $img = [
         "<img src='rrr.png' height='15'>",
         "<img src='s.jpg' height='15'>"
     ];
 
     return preg_replace($pat, $img, $text);
-  
 }
 
-function cens($text){
-    $pat="/дурак|идиот/";
-    if(preg_match($pat,$text)){
+function cens($text)
+{
+    $pat = "/дурак|идиот/";
+    if (preg_match($pat, $text)) {
         return "Ругаться не красиво!";
-    }else{
+    } else {
         return $text;
     }
+}
+
+function urls($text)
+{
+    $pat = [
+        // '/http(s)\:\/\/.*\.(png|jpg|gif)/i',
+        '/http(s)\:\/\/.*/i'
+
+    ];
+
+    $url = [
+        // "<img src='$0'>",
+        "<a href='$0'></a>"
+
+
+    ];
+    return preg_replace($pat, $url, $text);
 }
