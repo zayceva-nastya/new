a576f43bce8867868f2a041ef1d0bcb8111e2e26
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-03-10 16:09:14 +0300

    Create 17form.php

---
+++ lesson06.03/17form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="17.php" method="post">
+    <textarea name="url" id="" cols="30" rows="10"></textarea>
+    <input type="word">
+    <input type="submit">
+    
+    </form>
+</body>
+</html>
